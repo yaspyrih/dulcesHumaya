e8f04ff0478b9e04af0b8662b93e563b5446e179
yaspyrih <yaspyrih@gmail.com>
2023-09-12 15:13:58 -0300

    Adding recipes with videos through API

---
+++ index.js
@@ -0,0 +1,52 @@
+// const fetch = require('node-fetch');
+
+const url = 'https://tasty.p.rapidapi.com/recipes/list?from=1&size=3&q=caramel';
+const options = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '2c076e3027mshf86e989855d1f66p13fc5djsn8e66c7d0813e',
+    'X-RapidAPI-Host': 'tasty.p.rapidapi.com'
+  }
+};
+
+const cargarRecetas = async()=> {
+
+try {
+	const response = await fetch(url, options);
+	console.log(response);
+	if(response.status === 200){
+
+	const result = await response.json();
+	let recipes = "";
+
+	result.results.forEach(item => {
+		recipes += `
+		<div class="recipes">
+		<div class="row row-cols-md-3 w-100 m-auto bg-dark">
+		<div class="card text-bg-secondary">
+		<img class="thumbnail card-img-top" src="${item.thumbnail_url}">
+		<div class="card-body">
+		<h3 class="card-title name">${item.name}</h3>
+		<p id="description" class="description"> ${item.description}</p>
+		<a id="video" href="${item.video_url}" class="video btn btn-dark">Video</a>
+		</div>
+		</div>	
+		</div>
+		</div>
+	`;
+	})
+	document.getElementById("contenedor").innerHTML = recipes;
+
+}
+
+else if(response.status === 401){console.log("key incorrecta");}
+else if(response.status === 404){console.log("no disponible");}
+// else if{console.log("no tengo idea del error");} 
+}
+
+catch(error){
+console.log(error.message);
+}
+}
+
+cargarRecetas();
