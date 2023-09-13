2d4d51ca460bfa182b7fa7c0a16666a8ad4e6a69
yaspyrih <yaspyrih@gmail.com>
2023-09-12 21:04:09 -0300

    align api recipes

--- index.js
+++ index.js
@@ -21,17 +21,18 @@ try {
 
 	result.results.forEach(item => {
 		recipes += `
-		<div class="recipes">
-		<div class="row row-cols-md-3 w-100 m-auto bg-dark">
-		<div class="card text-bg-secondary">
-		<img class="thumbnail card-img-top" src="${item.thumbnail_url}">
-		<div class="card-body">
-		<h3 class="card-title name">${item.name}</h3>
-		<p id="description" class="description"> ${item.description}</p>
-		<a id="video" href="${item.video_url}" class="video btn btn-dark">Video</a>
-		</div>
-		</div>	
-		</div>
+		
+		<div class="recipes card-group rounded-start bg-dark">
+			<div class=" bg-dark">
+				<div class="card text-bg-secondary p-2 ">
+					<img class="thumbnail card-img-top rounded-start" src="${item.thumbnail_url}">
+						<div class="card-body">
+						<h3 class="card-title name">${item.name}</h3>
+						<p id="description" class="description"> ${item.description}</p>
+						<a id="video" href="${item.video_url}" class="video btn btn-dark">Video</a>
+						</div>
+				</div>	
+			</div>
 		</div>
 	`;
 	})
